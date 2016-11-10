86c77a447784b4f3302ab1682d315eabb95e4840
Yevgeniy Brikman <brikis98@gmail.com>
2016-11-10 14:01:18 +0000

    Add click tracking

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,31 @@
 (function() {
   "use strict";
+
+  var trackOutboundLink = function(event) {
+    var anchor = $(event.currentTarget);
+    var url = anchor.attr('href');
+    var trk = anchor.attr('data-trk');
+    var target = anchor.attr('target');
+    var valueString = anchor.attr('data-value');
+    var value = valueString ? parseInt(valueString, 10) : 0;
+
+    var props = {
+      'hitType': 'event',
+      'eventCategory': 'outbound',
+      'eventAction': 'click-' + trk,
+      'eventLabel': url,
+      'eventValue': value
+    };
+
+    if (target !== "_blank") {
+      event.preventDefault();
+      props['hitCallback'] = function() {
+        window.location = url;
+      };
+    }
+
+    ga('send', props);
+  };
+  
+  $('.tracked').on('click', trackOutboundLink);
 })();
